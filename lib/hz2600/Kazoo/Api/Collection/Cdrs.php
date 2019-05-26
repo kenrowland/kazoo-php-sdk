95084c2d59d57659a73c3945af642a4c6411f5b1
kenrowland <rowlandkenneth@gmail.com>
2019-05-26 18:14:08 -0400

    Add legs method to cdrs collection to return all legs for a call

--- lib/hz2600/Kazoo/Api/Collection/Cdrs.php
+++ lib/hz2600/Kazoo/Api/Collection/Cdrs.php
@@ -3,5 +3,9 @@ namespace Kazoo\Api\Collection;
 
 class Cdrs extends AbstractCollection
 {
-
+    public function legs($cdrId) {
+        $response = $this->get(array(), "/legs/$cdrId");
+        $this->setCollection($response->getData());
+        return $this;
+    }
 }
